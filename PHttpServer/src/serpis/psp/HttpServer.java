04821905f68c8621a14183f9875a96f3a54a0d21
juliana <jfreitassolano@gmail.com>
2013-02-04 09:44:33 +0100

    Implementado HttpServer con ThreadServer

--- PHttpServer/src/serpis/psp/HttpServer.java
+++ PHttpServer/src/serpis/psp/HttpServer.java
@@ -9,15 +9,28 @@ public class HttpServer {
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		// TODO Auto-generated method stub
 		final int port = 8080;
 		ServerSocket serverSocket = new ServerSocket(port);
+		//prueba multihilos
+		/*for (int i=0;i<4;i++){
+			Runnable runnable = new ThreadServer();
+			Thread thread = new Thread(runnable);
+			thread.start();
+			Thread.sleep(1000);
+			}*/
 		while(true)
 		{
 			Socket socket = serverSocket.accept();
-			SimpleServer.Process(socket);
+			//SimpleServer.Process(socket);
+			Runnable runnable = new ThreadServer(socket);
+			Thread thread = new Thread(runnable);
+			thread.start();
+			Thread.sleep(1000);
+			
 		}
 
 	}
--- PHttpServer/src/serpis/psp/ThreadServer.java
+++ PHttpServer/src/serpis/psp/ThreadServer.java
@@ -5,144 +5,155 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
-
-public class ThreadServer implements Runnable {
-	
-	private Socket socket = null;
+public class ThreadServer implements Runnable
+{
+	private Socket socket;
 	private static final String newLine = "\r\n";
+	private final String defaultFileName = "index.html";
+	private final String fileNameError404 = "fileError404.html";
+	private final String response200 ="HTTP/1.0 200 OK";
+	private final String response404 ="HTTP/1.0 404 Not Found";
 	
-	public ThreadServer(Socket socketCliente)
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	private String fileName;
+	private Boolean fileExists;
+	
+	public ThreadServer(Socket socket)
 	{
-		socket = socketCliente;
+		this.socket = socket;
 		
 	}
-	public static void main(String[] args) throws IOException
-	{
-		// TODO Auto-generated method stub
-		
-		final int port = 8080;
-		
-			ServerSocket serverSocket = null;
-			
-	        try {
-	        	serverSocket = new ServerSocket(port);
-	        } catch (IOException e)
-	        {
-	            System.err.println("No se puede escuchar en el puerto 8080.");
-	            System.exit(-1);
-	        }
-	        
-	        
-	        
-	
-	        while (true)
-	        {
-	        	
-	         
-			ThreadServer threadServer = new ThreadServer(serverSocket.accept());
-	          Thread hilo = new Thread (threadServer);
-	          hilo.start();
-	          
-	        }
-	        
-		}
-	
-		
 
-	
-		public void run()
-		{
-			// TODO Auto-generated method stub
-			
-			
-			try
-			{
-					String fileName = getFileName(socket.getInputStream());
-				 	//escribe la cabecera en funcion del archivo
-				 	writeHeader(socket.getOutputStream(),fileName);
-				 	writeFile(socket.getOutputStream(),fileName);
-				 	
-				 	socket.close();
-				 	
-				 	
+	@Override
+	public void run() {
+		/*for(int i=0;i<3;i++){
+			System.out.println(Thread.currentThread().getName() + "paso"+i);
+			try{
+				Thread.sleep(1000);
+			}catch(InterruptedException ex){
 				
-			}catch(Exception e){
-					System.err.println("Error:" + e.getMessage());
-					e.printStackTrace();
-					
-				}finally{
-				try {
-					socket.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				}
-	    	
-	    	}
-	    private static String getFileName(InputStream inputStream)
-		{
-			Scanner scanner = new Scanner( inputStream);
-			
-			String fileName = "index.html";
-			//se lee la peticion
-			while (true)
-			{
-				String line = scanner.nextLine();
-				if(line.startsWith("GET")){
-					
-				}
-				System.out.println(line);
-				if (line.equals(""))
-					break;
 			}
-			return fileName;
+		}*/
+		System.out.println(Thread.currentThread().getName() + "inicio.");
+		try{
+		inputStream = socket.getInputStream();
+		outputStream = socket.getOutputStream();
+		getFileName();
+	 	writeHeader();
+	 	writeFile();
+	 	getFileExists();
+	 	getFileName();
+	 	socket.close();
+		}catch(IOException ex){
+			
 		}
-		private static void writeHeader(OutputStream outputStream, String fileName) throws IOException
-		{	
-			File file = new File(fileName);
+		
+		System.out.println(Thread.currentThread().getName() + "fin.");
+		
+	}
+	
+	
+	private void getFileName()
+	{
+		Scanner scanner = new Scanner( inputStream);
+		
+		
+		//String fileName = "index.html";
+		String fileName = ""; 
+		
+		//se lee la peticion
+		while (true)
+		{
+			String line = scanner.nextLine();
 			
-			final String response200 ="HTTP/1.0 200 OK";
-			final String response404 ="HTTP/1.0 404 Not Found";
-			String response = file.exists() ? response200 : response404;
-			String header = response + newLine + newLine;
-			
-			byte[] headerBuffer = header.getBytes();
-			
-			outputStream.write(headerBuffer );
-		}
-		private static void writeFile(OutputStream outputStream, String fileName) throws IOException{
-			
-			final String fileNameError404 = "fileError404.html";
-			
-			File file = new File(fileName);
-			String responseFileName = file.exists() ? fileName : fileNameError404;
-			
-			final int bufferSize = 2048;
-			byte[] buffer = new byte[bufferSize];
-			
-			
-			FileInputStream fileInputStream = new FileInputStream(responseFileName);
-			
-			int count;
-			while((count = fileInputStream.read(buffer)) != -1){
+			if(line.startsWith("GET"))//Get /index.html HTTP/1.1
+			{		//opcion1
+				//fileName = line.split(" ")[1].substring(1);//->index.html
+				//System.out.println("fileName=" + fileName);
+					//opcion2
+				//fileName = line.substring(5,line.indexOf(" ",5));
+				//System.out.println("fileName=" + fileName);
+					//opcion3
+				/*int index = 5;
+				while (line.charAt(index) != ' '){
+					fileName += line.charAt(index++);
+					System.out.println("fileName=" + fileName);
+				}
+				System.out.println("fileName=" + fileName);*/
+					
+					//opcion5 MiOpcion
+				fileName = line.substring(line.indexOf(" ")+2, line.lastIndexOf(" "));
+				System.out.println("fileName=" + fileName);
+				//System.out.println("line.indexOf=" + line.indexOf(" "));
+				//System.out.println("line.LastindexOf =" + line.lastIndexOf(" "));
 				try {
-					Thread.sleep(3000);
+					Thread.sleep(2000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}//retardo del proceso de lectura y escritura
-				outputStream.write(buffer, 0, count);
-			}
+				}
 				
-			
-			fileInputStream.close();
+				
+			}
+			System.out.println(line);
+			if (line.equals(""))
+				break;
+		}
+		//cuando se pide el raiz:localhost:8080, aparece el fichero index.
+				if(fileName.equals(""))
+					fileName = defaultFileName;
+				System.out.println("fileName=" + fileName);
+		
+	}
+	private void getFileExists()
+	{
+		File file = new File(fileName);
+		fileExists = file.exists();
+	}
+	private  void writeHeader() throws IOException
+	{	
+		File file = new File(fileName);
+		
+		String response = file.exists() ? response200 : response404;
+		String header = response + newLine + newLine;
+		
+		byte[] headerBuffer = header.getBytes();
+		
+		outputStream.write(headerBuffer );
+	}
+	private  void writeFile() throws IOException{
+		
+		
+		
+		File file = new File(fileName);
+		String responseFileName = file.exists() ? fileName : fileNameError404;
+		
+		final int bufferSize = 2048;
+		byte[] buffer = new byte[bufferSize];
+		
+		
+		FileInputStream fileInputStream = new FileInputStream(responseFileName);
+		
+		int count;
+		while((count = fileInputStream.read(buffer)) != -1){
+			try {
+				System.out.println(Thread.currentThread().getName()+ ".");
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}//retardo del proceso de lectura y escritura
+			outputStream.write(buffer, 0, count);
 		}
 			
+		
+		fileInputStream.close();
+	}
+		
+	 
 
 }
